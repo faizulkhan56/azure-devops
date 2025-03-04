cd487ee8aa8417d32324848dfe979edbbd06d843
faizul.khan <faizul.khan@dngroup.com>
2025-03-04 12:14:10 +0600

    name change

--- devopswebapp/obj/Debug/net5.0/devopswebapp.AssemblyInfo.cs
+++ devopswebapp/obj/Debug/net5.0/devopswebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("devopswebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a6ea94e005f1b821c4ef0bc4660c3fc8789468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8cc87f2f56d51e0e18761375343cf1c29f9fa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("devopswebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("devopswebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
